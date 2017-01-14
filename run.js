c0c4d7425e10f56a47ed473bdcf09d9cbfaf7e64
Sebastian Patron <sebastian.patrones@gmail.com>
2017-01-14 10:30:26 -0700

    added print results func

--- run.js
+++ run.js
@@ -1,4 +1,7 @@
-var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+//const jsonArray[];
+let analysis;
+const results = [];
 
 var tone_analyzer = new ToneAnalyzerV3({
   username: 'fe96bc9f-d712-4c0e-abd1-9d1d6cc4c98d',
@@ -6,10 +9,26 @@ var tone_analyzer = new ToneAnalyzerV3({
   version_date: '2016-05-19'
 });
 
-tone_analyzer.tone({ text: 'Greetings from Watson Developer Cloud!' },
+tone_analyzer.tone({ text: 'I hate dislike  Watson Developer Cloud!' },
   function(err, tone) {
     if (err)
       console.log(err);
     else
-      console.log(JSON.stringify(tone, null, 2));
+      //console.log(JSON.stringify(tone, null, 2));
+      analysis = JSON.stringify(tone, null, 2);
+      //console.log(analysis);
+      printAnger(analysis);
 });
+
+function printAnger(jsonObj) {
+    JSON.parse(jsonObj, (key, value) => {
+        if (key == 'score') {
+          results.push(value);
+        }
+    })
+    //console.log(results);
+
+    results.forEach((value) => {
+      console.log("Watson says: " + value + "\n");
+    })
+}
